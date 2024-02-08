0963785b5bd883775efa36135ba1ff7485996987
jazellemaira <jazellemaira@pm.me>
2024-02-08 20:29:46 +0100

    Created Add-to-cart Function

--- store.js
+++ store.js
@@ -16,6 +16,12 @@ function ready() {
     var input = quantityInputs[i];
     input.addEventListener("change", quantityChanged);
   }
+
+  var addToCartButtons = document.getElementsByClassName("shop-item-btn");
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i];
+    button.addEventListener("click", addToCartClicked);
+  }
 }
 
 function removeCartItem(event) {
@@ -30,6 +36,58 @@ function quantityChanged(event) {
     input.value = 1;
   }
   updateCartTotal();
+}
+
+function addToCartClicked(event) {
+  var button = event.target;
+  var shopItem = button.parentElement.parentElement;
+  var title = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
+  var price = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
+  var imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src;
+  console.log(imageSrc, title, price);
+  addItemToCart(imageSrc, title, price);
+  updateCartTotal();
+}
+
+function addItemToCart(imageSrc, title, price) {
+  var cartRow = document.createElement("div");
+  cartRow.classList.add("cart-row");
+  var cartItems = document.getElementsByClassName("cart-items")[0];
+  var cartItemNames = cartItems.getElementsByClassName("cart-item-title");
+  for (var i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      alert("This item is already added to the cart");
+      return;
+    }
+  }
+  var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img
+              class="cart-item-image"
+              src="${imageSrc}"
+              alt="Tadhana T-shirt Design"
+              width="100"
+            />
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input
+              class="cart-quantity-input"
+              type="number"
+              value="1"
+              title="Item Quantity"
+            />
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`;
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+  cartRow
+    .getElementsByClassName("btn-danger")[0]
+    .addEventListener("click", removeCartItem);
+  cartRow
+    .getElementsByClassName("cart-quantity-input")[0]
+    .addEventListener("change", quantityChanged);
 }
 
 function updateCartTotal() {
